5b32ad49718597a8a83ec0de7b9aaa2f12a33106
ferdie bergado <ferdiebergado@gmail.com>
2024-12-14 11:00:42 +0800

    Refactored to use config

--- internal/pkg/http/html/html.go
+++ internal/pkg/http/html/html.go
@@ -3,23 +3,32 @@ package html
 import (
 	"bytes"
 	"embed"
-	"fmt"
 	"html/template"
 	"net/http"
 	"path/filepath"
 
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/config"
 	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/http/response"
 )
 
 //go:embed templates/*
 var templatesFS embed.FS
 
-func Render(w http.ResponseWriter, data any, templateFiles ...string) {
-	const (
-		templateDir      = "templates"
-		layoutFile       = "layout.html"
-		partialTemplates = "partials/*.html"
-	)
+type Template struct {
+	TemplateDir string
+	LayoutFile  string
+}
+
+func NewTemplate(cfg *config.HTMLTemplateConfig) *Template {
+	return &Template{
+		TemplateDir: cfg.TemplateDir,
+		LayoutFile:  cfg.LayoutFile,
+	}
+}
+
+func (t *Template) Render(w http.ResponseWriter, data any, templateFiles ...string) {
+	templateDir := t.TemplateDir
+	layoutFile := t.LayoutFile
 
 	layoutTemplate := filepath.Join(templateDir, layoutFile)
 
@@ -33,21 +42,21 @@ func Render(w http.ResponseWriter, data any, templateFiles ...string) {
 	templates, err := template.New("template").Funcs(getFuncMap()).ParseFS(templatesFS, targetTemplates...)
 
 	if err != nil {
-		response.RenderServerError(w, fmt.Errorf("parse template: %v", err))
+		response.RenderError(w, response.ServerError(err))
 		return
 	}
 
 	var buf bytes.Buffer
 
 	if err := templates.ExecuteTemplate(&buf, layoutFile, data); err != nil {
-		response.RenderServerError(w, fmt.Errorf("execute template: %v", err))
+		response.RenderError(w, response.ServerError(err))
 		return
 	}
 
 	_, err = buf.WriteTo(w)
 
 	if err != nil {
-		response.RenderServerError(w, fmt.Errorf("write to buffer: %v", err))
+		response.RenderError(w, response.ServerError(err))
 		return
 	}
 }

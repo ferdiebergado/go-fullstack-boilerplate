6e945ed5423332f6603030f626aeda0d9d1b5b70
ferdie bergado <ferdiebergado@gmail.com>
2024-12-25 11:05:10 +0800

    Moved run() to app package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,61 +4,14 @@ import (
 	"context"
 	"log/slog"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/app"
-	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/config"
-	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/db"
-	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/http/server"
-	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/logging"
-	"github.com/ferdiebergado/goexpress"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-// Run the application
-func run(ctx context.Context) error {
-	// Register OS Signal Listener
-	signalCtx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
-	// Load config
-	cfg := config.Load()
-
-	// Connect to the database.
-	database := db.New(cfg.DB)
-	conn, err := database.Connect(signalCtx)
-	if err != nil {
-		return err
-	}
-
-	defer database.Disconnect()
-
-	// Create the router
-	router := goexpress.New()
-
-	// Create the application
-	application := app.New(cfg, conn, router)
-	application.SetupRouter()
-
-	// Start the httpServer
-	httpServer := server.New(cfg.Server, router)
-	if err := httpServer.Start(signalCtx); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func main() {
-	logging.SetLogger()
-
-	slog.Info("Running application...")
-
-	if err := run(context.Background()); err != nil {
+	if err := app.Run(context.Background()); err != nil {
 		slog.Error("Fatal error occurred.", "reason", err, "severity", "Fatal")
 		os.Exit(1)
 	}
-
-	slog.Info("Done.")
 }
---
+++ internal/app/run.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/config"
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/db"
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/http/server"
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/logging"
+	"github.com/ferdiebergado/goexpress"
+)
+
+// Run the application
+func Run(ctx context.Context) error {
+	// Set the logger
+	logging.SetLogger()
+	slog.Info("Running application...")
+
+	// Register OS Signal Listener
+	signalCtx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// Load config
+	cfg := config.Load()
+
+	// Connect to the database.
+	database := db.New(cfg.DB)
+	conn, err := database.Connect(signalCtx)
+	if err != nil {
+		return err
+	}
+
+	defer database.Disconnect()
+
+	// Create the router
+	router := goexpress.New()
+
+	// Create the application
+	application := New(cfg, conn, router)
+	application.SetupRouter()
+
+	// Start the httpServer
+	httpServer := server.New(cfg.Server, router)
+	if err := httpServer.Start(signalCtx); err != nil {
+		return err
+	}
+
+	slog.Info("Done.")
+
+	return nil
+}

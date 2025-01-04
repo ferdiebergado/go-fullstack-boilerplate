879eb5dbd801ac09a3f4fd82c7d96e95c000ec4b
ferdie bergado <ferdiebergado@gmail.com>
2025-01-04 20:03:13 +0800

    Separate unit tests from integration tests

--- test/integration/auth_handler_test.go
+++ test/integration/auth_handler_test.go
@@ -1,4 +1,6 @@
-package app
+//go:build integration
+
+package integration
 
 import (
 	"context"
@@ -9,6 +11,8 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/app"
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/app/user"
 	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/config"
 	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/db"
 	"github.com/ferdiebergado/goexpress"
@@ -16,7 +20,7 @@ import (
 	"github.com/ferdiebergado/gopherkit/assert"
 )
 
-func TestBaseHandler(t *testing.T) {
+func TestAuthHandler(t *testing.T) {
 	cfg := config.Load()
 
 	database := db.New(cfg.DB)
@@ -29,26 +33,26 @@ func TestBaseHandler(t *testing.T) {
 	defer database.Disconnect()
 
 	router := goexpress.New()
-	application := New(cfg, conn, router)
+	application := app.New(cfg, conn, router)
 	application.SetupRouter()
 
-	t.Run("GET /api/health should return status 200 and render json", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodGet, "/api/health", nil)
+	t.Run("POST /api/signup should return status 201 and send new user as json ", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/api/signup", nil)
 		rec := httptest.NewRecorder()
 
 		router.ServeHTTP(rec, req)
 
 		assert.Equal(t, http.StatusOK, rec.Code)
 
-		var result HealthResponse
-		err := json.NewDecoder(rec.Body).Decode(&result)
+		var user user.User
+		err := json.NewDecoder(rec.Body).Decode(&user)
 
 		assert.NoError(t, err)
-		assert.Equal(t, "healthy", result.Status)
+		// assert.Equal(t, "healthy", result.Status)
 	})
 
-	t.Run("GET /nonexistent should return status 404 and render 404.html", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodGet, "/nonexistent", nil)
+	t.Run("GET /signup should return status 200 and render signin.html", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/signup", nil)
 		rec := httptest.NewRecorder()
 
 		router.ServeHTTP(rec, req)
---
+++ test/integration/basehandler_test.go
@@ -0,0 +1,66 @@
+//go:build integration
+
+package integration
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/app"
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/config"
+	"github.com/ferdiebergado/go-fullstack-boilerplate/internal/pkg/db"
+	"github.com/ferdiebergado/goexpress"
+
+	"github.com/ferdiebergado/gopherkit/assert"
+)
+
+func TestBaseHandler(t *testing.T) {
+	cfg := config.Load()
+
+	database := db.New(cfg.DB)
+	conn, err := database.Connect(context.Background())
+
+	if err != nil {
+		t.Fatalf("can't connect to the database: %v", err)
+	}
+
+	defer database.Disconnect()
+
+	router := goexpress.New()
+	application := app.New(cfg, conn, router)
+	application.SetupRouter()
+
+	t.Run("GET /api/health should return status 200 and render json", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/api/health", nil)
+		rec := httptest.NewRecorder()
+
+		router.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		var result app.HealthResponse
+		err := json.NewDecoder(rec.Body).Decode(&result)
+
+		assert.NoError(t, err)
+		assert.Equal(t, "healthy", result.Status)
+	})
+
+	t.Run("GET /nonexistent should return status 404 and render 404.html", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/nonexistent", nil)
+		rec := httptest.NewRecorder()
+
+		router.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusNotFound, rec.Code)
+
+		expected := "The page you were looking for does not exist."
+		actual := rec.Body.String()
+
+		assert.Contains(t, actual, expected)
+	})
+}

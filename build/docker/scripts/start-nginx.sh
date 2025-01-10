568bc7dd6cff4fd3165bf7e4a7069e977c71d33b
ferdie bergado <ferdiebergado@gmail.com>
2025-01-10 21:53:32 +0800

    Moved nginx configs to nginx subfolder

--- build/docker/scripts/start-nginx.sh
+++ build/docker/scripts/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Inject Environment Variables into Nginx Config
-envsubst '$SERVER_PORT' </etc/nginx/nginx.conf.template >/etc/nginx/nginx.conf
+envsubst '$SERVER_PORT' </etc/nginx/conf.d/nginx.conf.template >/etc/nginx/nginx.conf
 
 # Start Nginx
 exec nginx -g 'daemon off;'

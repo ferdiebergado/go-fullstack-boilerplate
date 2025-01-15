eef9e18f89c475d653643b3175fbbdf99995e1d8
ferdie bergado <ferdiebergado@gmail.com>
2025-01-15 16:24:16 +0800

    Moved const and vars outside of function

--- internal/pkg/validation/validation.go
+++ internal/pkg/validation/validation.go
@@ -2,11 +2,11 @@ package validation
 
 import "regexp"
 
+const emailRegex = `^(?i:[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)$`
+
+var re = regexp.MustCompile(emailRegex)
+
 // Validates an email address.
 func IsValidEmail(email string) bool {
-	const emailRegex = `^(?i:[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)$`
-
-	re := regexp.MustCompile(emailRegex)
-
 	return re.MatchString(email)
 }

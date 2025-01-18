0b7e1dcb455119a6cd022cfbbaee7df5438c88d9
ferdie bergado <ferdiebergado@gmail.com>
2025-01-18 22:03:39 +0800

    Added FromContext() function

--- internal/app/user/context.go
+++ internal/app/user/context.go
@@ -1,11 +1,25 @@
 package user
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type ctxKey string
 
 const userKey ctxKey = "userID"
 
+var ErrUserNotInContext = errors.New("no user in context")
+
 func WithUser(ctx context.Context, userID string) context.Context {
 	return context.WithValue(ctx, userKey, userID)
+}
+
+func FromContext(ctx context.Context) (string, error) {
+	userID, ok := ctx.Value(userKey).(string)
+	if !ok {
+		return "", ErrUserNotInContext
+	}
+
+	return userID, nil
 }

bb73a7ae02c7f84bedacf7132e69af83d5d903cd
ferdie bergado <ferdiebergado@gmail.com>
2025-01-26 20:21:54 +0800

    Fixed incorrect table name

--- db/migrations/000004_create_sessions_table.down.sql
+++ db/migrations/000004_create_sessions_table.down.sql
@@ -2,4 +2,4 @@ DROP INDEX IF EXISTS idx_user_sessions_user_id;
 DROP INDEX IF EXISTS idx_user_sessions_last_activity;
 DROP INDEX IF EXISTS idx_user_sessions_expiry_time;
 
-DROP TABLE IF EXISTS user_sessions;
+DROP TABLE IF EXISTS sessions;
